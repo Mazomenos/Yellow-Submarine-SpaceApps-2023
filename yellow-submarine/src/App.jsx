ca675d4034ffabfa880f48fda035a745ab02e23c
Jaza2401 <alex.jauregui.zarate@gmail.com>
2023-10-07 12:41:40 -0700

    added: some little changes to the textboxes

--- yellow-submarine/src/App.jsx
+++ yellow-submarine/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Parallax pages={1.5} style={{ top: '0', left: '0' }} class="animation">
+      <Parallax pages={3} style={{ top: '0', left: '0' }} class="animation">
         <ParallaxLayer offset={0} speed={0.2}>
           <div class="animation_layer parallax" id="artback"></div>
         </ParallaxLayer>
@@ -20,7 +20,7 @@ function App() {
         <ParallaxLayer offset={.7} speed={0.40}>
           <TextBlock/>
         </ParallaxLayer>
-        <ParallaxLayer offset={1.2} speed={0.4}>
+        <ParallaxLayer offset={1.3} speed={0.4}>
           <TextBox/>
         </ParallaxLayer>
       </Parallax>
--- yellow-submarine/src/Components/textBox.jsx
+++ yellow-submarine/src/Components/textBox.jsx
@@ -1,15 +1,13 @@
-import "../App.css"
+import "../App.css";
 
 function TextBox() {
     return (
         <div id="textbox">
-            <div id="textbox-container">
-                <h1 id="textbox-title">What is Firewatch?</h1>
+            <div id="textblock-container">
+                <h1 id="textblock-title">What is Firewatch?</h1>
                 <p id="textbox-content">
                 The year is 1989.<br/><br/>
-                You are a man named Henry who has retreated from your messy life to work as a fire lookout in the Wyoming wilderness. Perched atop a mountain, it's your job to find smoke and keep the wilderness safe.<br/><br/>
-                An especially hot, dry summer has everyone on edge. Your supervisor, a woman named Delilah, is available to youat all times over a small, handheld radio—and is your only contact with the world you've left behind.<br/><br/>
-                But when something strange draws you out of your lookout tower and into the world below, you'll explore a wild and unknown environment, facing questions and making interpersonal choices that can build or destroy the only meaningful relationship you have.
+                You are a man named Henry who has retreated from your messy life to work as a fire lookout in the Wyoming wilderness. Perched atop a mountain, it's your job to find smoke and keep the wilderness safe.
                 </p>
             </div>
             <footer id='textblock-footer'>Demo<a id="textblock-devsense" href="https://youtube.com/c/DevSense19">DevSense</a></footer>
