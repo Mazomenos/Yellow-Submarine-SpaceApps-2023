9da395f67dc4e4727b55f497ed09e17aa7ba794a
Jaza2401 <alex.jauregui.zarate@gmail.com>
2023-10-07 12:18:51 -0700

    add: file path for components, added few example components

--- yellow-submarine/src/App.jsx
+++ yellow-submarine/src/App.jsx
@@ -1,24 +1,27 @@
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import TextBox from './Components/textBox';
 import TextBlock from './Components/textBlock';
 import './App.css';
 
 
 function App() {
-  
   return (
     <div className="App">
       <Parallax pages={1.5} style={{ top: '0', left: '0' }} class="animation">
-        <ParallaxLayer offset={0} speed={0.25}>
+        <ParallaxLayer offset={0} speed={0.2}>
           <div class="animation_layer parallax" id="artback"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.3}>
           <div class="animation_layer parallax" id="jungle1"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.40}>
+        <ParallaxLayer offset={0} speed={0.4}>
           <div class="animation_layer parallax" id="manonmountain"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={.7} speed={0.40}>
-        <TextBlock />
+          <TextBlock/>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.2} speed={0.4}>
+          <TextBox/>
         </ParallaxLayer>
       </Parallax>
     </div>
